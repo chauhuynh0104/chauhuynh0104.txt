25c688ffd7eabbf5dec5758164bcbb40595a9207
chauhuynh0104 <chauhuynh0104@gmail.com>
2025-07-13 17:35:11 +0700

    Add files via upload

---
+++ deepseek_javascript_20250713_976973.js
@@ -0,0 +1,5 @@
+// File này đặt trên GitHub
+const REMOTE_CONFIG = {
+  ACCESS_ALLOWED: false, // Thay đổi giá trị này để chặn/mở truy cập
+  REDIRECT_URL: "https://google.com" // URL chuyển hướng khi bị chặn
+};
